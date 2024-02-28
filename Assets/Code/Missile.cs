37ade16d9e02cb0dd70db645d59ca2bd4da68035
Александр Чумаченко <AC.Sasha20041994@gmail.com>
2024-02-28 19:51:03 +0300

    some code

--- Assets/Code/Missile.cs
+++ Assets/Code/Missile.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+
+    public int speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.GetComponent<Enemy>().gameObject);
+        Destroy(gameObject);
     }
 }
