93fe2013abad452f2c59910f569109a9e65de769
Александр Чумаченко <AC.Sasha20041994@gmail.com>
2024-02-28 18:59:28 +0300

    init

---
+++ Assets/Code/SpeedTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTriger : MonoBehaviour
+{
+
+    public float speedFactor = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<FirstPersonMovement> ().runSpeed *= speedFactor;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<FirstPersonMovement>().runSpeed /= speedFactor;
+    }
+}
